3cfd95a7ceaab06e8af051f9a02a93c4b9cc2c6d
ChococupC <Qiaokebei123@gmail.com>
2025-03-22 16:27:39 -0400

    Add files via upload

---
+++ js/cookie.js
@@ -0,0 +1,29 @@
+function getCookie(name) {
+  let cookieName = name + "=";
+  let decodedCookie = decodeURIComponent(document.cookie);
+  let cookiesArray = decodedCookie.split(";");
+
+  for (let i = 0; i < cookiesArray.length; i++) {
+    let cookie = cookiesArray[i].trim();
+
+    if (cookie.indexOf(cookieName) === 0) {
+      return cookie.substring(cookieName.length, cookie.length);
+    }
+  }
+
+  return null;
+}
+
+function setCookie(name, value, expireDate) {
+  document.cookie = `${name}=${value};expires=${expireDate.toUTCString()};path=/`;
+}
+
+function setCookieMidnight(name, value) {
+  let tomorrow = new Date();
+
+  tomorrow.setDate(tomorrow.getDate() + 1);
+  tomorrow.setHours(0, 0, 0, 0);
+
+  console.log(name, value, tomorrow);
+  setCookie(name, value, tomorrow);
+}
