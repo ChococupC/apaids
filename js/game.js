cdc5c35e46daf15be17ad2e74dd6b444d52f640b
ChococupC <Qiaokebei123@gmail.com>
2025-03-24 12:55:08 -0400

    Update game.js

--- js/game.js
+++ js/game.js
@@ -57,7 +57,7 @@ function resizeWords() {
       var textWidth = window.matchMedia(
         "only screen and (max-width: 64em) and (orientation: landscape)"
       ).matches
-        ? this.scrollHeight + 6
+        ? this.scrollHeight - 5
         : this.scrollWidth;
 
       if (textWidth > containerWidth) {
